9e7723ae0a5f69f9657c0dcb6017072c015daa39
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2022-10-15 18:27:03 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base:'/e-commerce/',
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
     },
